69cfbb7fe33910124b557f108f7b8dafeac09c01
Vikky Shostak <vikkyshostak@gmail.com>
2017-10-29 23:32:04 +0300

    Update docs, add .editorconfig

--- src/VueGoodshare.vue
+++ src/VueGoodshare.vue
@@ -1,23 +1,19 @@
 <template>
-    <div>
-        <vue-goodshare-facebook
-            page_url="https://github.com" has_icon has_counter title_social="Facebook">
-        </vue-goodshare-facebook>
-        <vue-goodshare-vkontakte
-            page_url="https://github.com" has_icon has_counter title_social="Вконтакте">
-        </vue-goodshare-vkontakte>
-    </div>
+  <div>
+    <vue-goodshare-facebook has_icon has_counter title_social="Facebook"></vue-goodshare-facebook>
+    <vue-goodshare-vkontakte has_icon has_counter title_social="Вконтакте"></vue-goodshare-vkontakte>
+  </div>
 </template>
 
 <script>
-    import VueGoodshareVkontakte from "./providers/Vkontakte.vue"
-    import VueGoodshareFacebook from "./providers/Facebook.vue"
-    
-    export default {
-        name: 'VueGoodshare',
-        components: {
-            VueGoodshareFacebook,
-            VueGoodshareVkontakte
-        }
+  import VueGoodshareVkontakte from "./providers/Vkontakte.vue"
+  import VueGoodshareFacebook from "./providers/Facebook.vue"
+  
+  export default {
+    name: 'VueGoodshare',
+    components: {
+      VueGoodshareFacebook,
+      VueGoodshareVkontakte
     }
+  }
 </script>
