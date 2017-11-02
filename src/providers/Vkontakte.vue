46ec38dd6d2637c046a8db0a50fb771c1f8c7d87
Vic Shóstak <koddr.me@gmail.com>
2017-11-03 01:40:42 +0300

    Add all social icons from Fontello

--- src/providers/Vkontakte.vue
+++ src/providers/Vkontakte.vue
@@ -149,12 +149,12 @@
   // Fontello
   @font-face {
     font-family: 'Fontello';
-    src: url('../fonts/fontello.eot?80371804');
-    src: url('../fonts/fontello.eot?80371804#iefix') format('embedded-opentype'),
-    url('../fonts/fontello.woff2?80371804') format('woff2'),
-    url('../fonts/fontello.woff?80371804') format('woff'),
-    url('../fonts/fontello.ttf?80371804') format('truetype'),
-    url('../fonts/fontello.svg?80371804#fontello') format('svg');
+    src: url('../fonts/fontello.eot?88483298');
+    src: url('../fonts/fontello.eot?88483298#iefix') format('embedded-opentype'),
+    url('../fonts/fontello.woff2?88483298') format('woff2'),
+    url('../fonts/fontello.woff?88483298') format('woff'),
+    url('../fonts/fontello.ttf?88483298') format('truetype'),
+    url('../fonts/fontello.svg?88483298#fontello') format('svg');
     font-weight: normal;
     font-style: normal;
   }
