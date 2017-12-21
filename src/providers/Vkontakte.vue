e8a0459a687340f5c5ab8f45ad911d0a71a58221
iohansson <w.iohansson@gmail.com>
2017-12-21 16:39:51 +0300

    fix counter update when multiple instances are used

--- src/providers/Vkontakte.vue
+++ src/providers/Vkontakte.vue
@@ -109,12 +109,23 @@
         return window.open(share_url, 'Share this', window_config + 'toolbar=no,menubar=no,scrollbars=no')
       },
       
+      handleUpdateCount(count) {
+        this.counter_vkontakte = (count >= 1000)
+          ? this.sliceThousandInt(count)
+          : count;
+      },
+
       /**
        * Get share counter.
        *
        * @return {object} a share counter
        */
       getShareCounter: function () {
+        // Let's see whether some other component has already
+        // asked for count. Then we just subscribe for the count update event
+        if (window.VK && window.VK.Share && typeof window.VK.Share.count === 'function') {
+          return;
+        }
         // Variables
         const script = document.createElement('script')
         
@@ -131,16 +142,16 @@
         window.VK = Object.assign({}, { Share: {} }, window.VK);
         window.VK.Share.count = (index, count) => {
           if (count) {
-            this.counter_vkontakte = (count >= 1000)
-              ? this.sliceThousandInt(count)
-              : count
+            this.$root.$emit('VK:Share:count:update', count);
           }
         }
       }
     },
     mounted () {
       // Show share counter when page loaded
-      if (this.$props.has_counter) this.getShareCounter()
+      if (this.$props.has_counter) this.getShareCounter();
+      // Subscribe to update count event using $root as an event bus
+      this.$root.$on('VK:Share:count:update', this.handleUpdateCount.bind(this));
     }
   }
 </script>
